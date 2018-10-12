4ab2941f8291882ad76f8d7e10a093a1a46a36b0
COPIN Thomas <TCOPIN@micropole.com>
2018-10-12 10:47:52 +0200

    Import of Travel Component and passing him props value

--- my-travels/src/App.js
+++ my-travels/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { Row, Col } from 'reactstrap'
 import './App.css'
+
+import Travel from './Travel'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
+          <Row>
+            <Col xs='12' md='6' lg='6'>
+              <Travel
+                photo='https://www.kuoni.fr/wp-content/uploads/2017/12/IS-Circuit-Islande-%C2%A9-Nikolay-Pandev-iStock-56258174-500x500.jpg'
+                destination='Islande'
+                country='Islande'
+                distance='2038 km'
+              />
+            </Col>
+            <Col xs='12' md='6' lg='6'>
+              <Travel
+                photo='https://i0.wp.com/www.carnetdeshopping.com/wp-content/uploads/2011/07/plages-de-guadeloupe_500.jpg?fit=500%2C500&ssl=1'
+                destination='Guadeloupe'
+                country='Guadeloupe'
+                distance='6831 km'
+              />
+            </Col>
+          </Row>
         </header>
       </div>
     )
