c12d6c1a20eabf3ba0a9dd2321c303816eb7d37e
COPIN Thomas <TCOPIN@micropole.com>
2018-10-16 17:13:28 +0200

    WIP on Simpsons-quote

--- simpsons-quote/src/App.js
+++ simpsons-quote/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import Quote from './Quote'
+import Quotes from './Quotes'
+import Lamp from './Lamp'
 
 class App extends Component {
   render() {
@@ -12,16 +13,9 @@ class App extends Component {
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Simpsons Quotes</h1>
         </header>
-        <Quote
-          quote='I believe the children are the future... Unless we stop them now!'
-          character='Homer Simpson'
-          image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939'
-        />
-        <Quote
-          quote="Me fail English? That's unpossible"
-          character='Ralph Wiggum'
-          image='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523'
-        />
+        <Lamp on />
+        <Lamp />
+        <Quotes />
       </div>
     )
   }
